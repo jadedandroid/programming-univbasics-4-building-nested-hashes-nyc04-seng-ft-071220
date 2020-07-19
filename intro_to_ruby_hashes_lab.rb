c23fa19ae4f1da9e6dbda122c99d998c42121d6c
jade decordova <lacroixjaded@gmail.com>
2020-07-19 02:47:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-   hash = {:railroads => {:pieces => 4 }, {:rent_in_dollars => {}}, {:names => {}}}
+   hash = {:railroads => {:pieces => 4 }, 
+          {:rent_in_dollars => {}}, 
+          {:names => {}}
+     
+   }
    hash
   
 end
