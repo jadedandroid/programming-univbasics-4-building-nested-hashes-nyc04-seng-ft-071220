46afefeda3a8d2f254145fb1d231775bb30a813c
jade decordova <lacroixjaded@gmail.com>
2020-07-19 02:53:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,12 @@ end
 
 def monopoly_with_third_tier
   hash = {:railroads => {:pieces => 4, 
-          :rent_in_dollars => {}, 
+          :rent_in_dollars =>
+          {:one_piece_owned => 50, 
+           :two_piece_owned => 100,
+           :three_piece_owned => 150,
+           :four_piece_owned => 200
+          }, 
           :names => {}}
      
    }
